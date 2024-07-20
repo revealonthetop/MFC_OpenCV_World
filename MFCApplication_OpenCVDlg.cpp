3ef21a24b67db046fbc08cf13eb76933fdfda462
revealontop <revealontop@gmail.com>
2024-07-21 00:43:30 +0900

    git_publish
    project_code_publish

---
+++ MFCApplication_OpenCVDlg.cpp
@@ -0,0 +1,519 @@
+﻿
+// MFCApplication_OpenCVDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFCApplication_OpenCV.h"
+#include "MFCApplication_OpenCVDlg.h"
+#include "afxdialogex.h"
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
+#include <iostream>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+using namespace cv;
+using namespace std;
+
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+
+private:
+	string m_imagePath;
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CMFCApplicationOpenCVDlg 대화 상자
+
+
+CMFCApplicationOpenCVDlg::CMFCApplicationOpenCVDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFCAPPLICATION_OPENCV_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCApplicationOpenCVDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMFCApplicationOpenCVDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_STN_CLICKED(IDC_STATIC_TEXT, &CMFCApplicationOpenCVDlg::OnStnClickedStaticText)
+	ON_BN_CLICKED(IDC_BUTTON_IMGOPEN, &CMFCApplicationOpenCVDlg::OnBnClickedButtonImgopen)
+	ON_BN_CLICKED(IDC_BUTTON_VIDEO_OPEN, &CMFCApplicationOpenCVDlg::OnBnClickedButtonVideoOpen)
+	ON_BN_CLICKED(IDC_BUTTON_COLOR_PICKER, &CMFCApplicationOpenCVDlg::OnBnClickedButtonColorPicker)
+	ON_BN_CLICKED(IDC_BUTTON_LICENSE_PLATE_RECOGNITION, &CMFCApplicationOpenCVDlg::OnBnClickedButtonLicensePlateRecognition)
+	ON_BN_CLICKED(IDC_BUTTONDOCUMENT_SCANNER, &CMFCApplicationOpenCVDlg::OnBnClickedButtondocumentScanner)
+	ON_BN_CLICKED(IDC_BUTTON_VIRTUAL_PAINT, &CMFCApplicationOpenCVDlg::OnBnClickedButtonVirtualPaint)
+	ON_BN_CLICKED(IDC_BUTTON_FACE_RECOGITION, &CMFCApplicationOpenCVDlg::OnBnClickedButtonFaceRecogition)
+END_MESSAGE_MAP()
+
+
+// CMFCApplicationOpenCVDlg 메시지 처리기
+
+BOOL CMFCApplicationOpenCVDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CMFCApplicationOpenCVDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 애플리케이션의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CMFCApplicationOpenCVDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CMFCApplicationOpenCVDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+void CMFCApplicationOpenCVDlg::OnStnClickedStaticText()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+}
+
+
+void CMFCApplicationOpenCVDlg::OnBnClickedButtonImgopen()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	//string path = "Resources/test.png"; test code - 성공~~~
+	CFileDialog fileDlg(TRUE, _T("png"), NULL, OFN_FILEMUSTEXIST | OFN_HIDEREADONLY, _T("Image Files (*.png; *.jpg; *.jpeg; *.bmp)|*.png;*.jpg;*.jpeg;*.bmp||"));
+
+	// 대화 상자를 표시하고 사용자가 파일을 선택하면 경로를 가져옵니다.
+	if (fileDlg.DoModal() == IDOK)
+	{
+		CString filePath = fileDlg.GetPathName(); // 사용자가 선택한 파일 경로를 가져옵니다.
+		string path = CT2A(filePath.GetString()); // CString을 std::string으로 변환
+
+		Mat img = imread(path);
+		if (img.empty())
+		{
+			AfxMessageBox(_T("이미지를 로드할 수 없습니다. 올바른 파일을 선택했는지 확인하세요."));
+			return;
+		}
+		imshow("Image", img);
+		waitKey(0);
+	}
+}
+
+void CMFCApplicationOpenCVDlg::OnBnClickedButtonVideoOpen()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+}
+
+
+void CMFCApplicationOpenCVDlg::OnBnClickedButtonColorPicker()
+{
+	string path = "Resources/lambo.png";
+	Mat img = imread(path);
+	Mat imgHSV, mask;
+	int hmin = 0, smin = 110, vmin = 153;
+	int hmax = 19, smax = 240, vmax = 255;
+
+	cvtColor(img, imgHSV, COLOR_BGR2HSV);
+
+	namedWindow("Trackbars", (640, 200));
+	createTrackbar("Hue Min", "Trackbars", &hmin, 179);
+	createTrackbar("Hue Max", "Trackbars", &hmax, 179);
+	createTrackbar("Sat Min", "Trackbars", &smin, 255);
+	createTrackbar("Sat Max", "Trackbars", &smax, 255);
+	createTrackbar("Val Min", "Trackbars", &vmin, 255);
+	createTrackbar("Val Max", "Trackbars", &vmax, 255);
+
+	while (true) {
+
+		Scalar lower(hmin, smin, vmin);
+		Scalar upper(hmax, smax, vmax);
+		inRange(imgHSV, lower, upper, mask);
+
+		imshow("Image", img);
+		imshow("Image HSV", imgHSV);
+		imshow("Image Mask", mask);
+		int key = waitKey(1);
+		if (key == 'q' || key == 27) // 'q' 키 또는 'Esc' 키를 누르면 루프를 종료
+		{
+			break;
+		}
+	}
+	destroyAllWindows();
+}
+
+
+void CMFCApplicationOpenCVDlg::OnBnClickedButtonLicensePlateRecognition()
+{
+		Mat img;
+		VideoCapture cap(0);
+
+		CascadeClassifier plateCascade;
+		plateCascade.load("Resources/haarcascade_russian_plate_number.xml");
+
+		if (plateCascade.empty()) { cout << "XML file not loaded" << endl; }
+
+		vector<Rect> plates;
+
+		while (true) {
+
+			cap.read(img);
+			plateCascade.detectMultiScale(img, plates, 1.1, 10);
+
+			for (int i = 0; i < plates.size(); i++)
+			{
+				Mat imgCrop = img(plates[i]);
+				//imshow(to_string(i), imgCrop);
+				imwrite("Resources/Plates/" + to_string(i) + ".png", imgCrop);
+				rectangle(img, plates[i].tl(), plates[i].br(), Scalar(255, 0, 255), 3);
+			}
+
+			imshow("Image", img);
+			int key = waitKey(1);
+			if (key == 'q' || key == 27) // 'q' 키 또는 'Esc' 키를 누르면 루프를 종료
+			{
+				break;
+			}
+		}
+		destroyAllWindows();
+	}
+
+Mat imgOriginal, imgGray, imgBlur, imgCanny, imgThre, imgDil, imgErode, imgWarp, imgCrop;
+vector<Point> initialPoints, docPoints;
+float w = 420, h = 596;
+
+Mat preProcessing(Mat img)
+{
+	cvtColor(img, imgGray, COLOR_BGR2GRAY);
+	GaussianBlur(imgGray, imgBlur, Size(3, 3), 3, 0);
+	Canny(imgBlur, imgCanny, 25, 75);
+	Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+	dilate(imgCanny, imgDil, kernel);
+	//erode(imgDil, imgErode, kernel);
+	return imgDil;
+}
+
+vector<Point> getContours(Mat image) {
+
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+
+	findContours(image, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+	//drawContours(img, contours, -1, Scalar(255, 0, 255), 2);
+	vector<vector<Point>> conPoly(contours.size());
+	vector<Rect> boundRect(contours.size());
+
+	vector<Point> biggest;
+	int maxArea = 0;
+
+	for (int i = 0; i < contours.size(); i++)
+	{
+		int area = contourArea(contours[i]);
+		//cout << area << endl;
+
+		string objectType;
+
+		if (area > 1000)
+		{
+			float peri = arcLength(contours[i], true);
+			approxPolyDP(contours[i], conPoly[i], 0.02 * peri, true);
+
+			if (area > maxArea && conPoly[i].size() == 4) {
+
+				//drawContours(imgOriginal, conPoly, i, Scalar(255, 0, 255), 5);
+				biggest = { conPoly[i][0],conPoly[i][1] ,conPoly[i][2] ,conPoly[i][3] };
+				maxArea = area;
+			}
+			//drawContours(imgOriginal, conPoly, i, Scalar(255, 0, 255), 2);
+			//rectangle(imgOriginal, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 5);
+		}
+	}
+	return biggest;
+}
+
+void drawPoints(vector<Point> points, Scalar color)
+{
+	for (int i = 0; i < points.size(); i++)
+	{
+		circle(imgOriginal, points[i], 10, color, FILLED);
+		putText(imgOriginal, to_string(i), points[i], FONT_HERSHEY_PLAIN, 4, color, 4);
+	}
+}
+
+vector<Point> reorder(vector<Point> points)
+{
+	vector<Point> newPoints;
+	vector<int>  sumPoints, subPoints;
+
+	for (int i = 0; i < 4; i++)
+	{
+		sumPoints.push_back(points[i].x + points[i].y);
+		subPoints.push_back(points[i].x - points[i].y);
+	}
+
+	newPoints.push_back(points[min_element(sumPoints.begin(), sumPoints.end()) - sumPoints.begin()]); // 0
+	newPoints.push_back(points[max_element(subPoints.begin(), subPoints.end()) - subPoints.begin()]); //1
+	newPoints.push_back(points[min_element(subPoints.begin(), subPoints.end()) - subPoints.begin()]); //2
+	newPoints.push_back(points[max_element(sumPoints.begin(), sumPoints.end()) - sumPoints.begin()]); //3
+
+	return newPoints;
+}
+
+Mat getWarp(Mat img, vector<Point> points, float w, float h)
+{
+	Point2f src[4] = { points[0],points[1],points[2],points[3] };
+	Point2f dst[4] = { {0.0f,0.0f},{w,0.0f},{0.0f,h},{w,h} };
+
+	Mat matrix = getPerspectiveTransform(src, dst);
+	warpPerspective(img, imgWarp, matrix, Point(w, h));
+
+	return imgWarp;
+}
+
+
+void CMFCApplicationOpenCVDlg::OnBnClickedButtondocumentScanner()
+{
+	string path = "Resources/paper.jpg";
+	imgOriginal = imread(path);
+	//resize(imgOriginal, imgOriginal, Size(), 0.5, 0.5);
+
+	// Preprpcessing - Step 1 
+	imgThre = preProcessing(imgOriginal);
+
+	// Get Contours - Biggest  - Step 2
+	initialPoints = getContours(imgThre);
+	//drawPoints(initialPoints, Scalar(0, 0, 255));
+	docPoints = reorder(initialPoints);
+	//drawPoints(docPoints, Scalar(0, 255, 0));
+
+	// Warp - Step 3 
+	imgWarp = getWarp(imgOriginal, docPoints, w, h);
+
+	//Crop - Step 4
+	int cropVal = 5;
+	Rect roi(cropVal, cropVal, w - (2 * cropVal), h - (2 * cropVal));
+	imgCrop = imgWarp(roi);
+
+	imshow("Image", imgOriginal);
+	//imshow("Image Dilation", imgThre);
+	//imshow("Image Warp", imgWarp);
+	imshow("Image Crop", imgCrop);
+	waitKey(0);
+}
+
+Mat img;
+VideoCapture cap(0);
+vector<vector<int>> newPoints;  // to store all points
+
+/////////////////////  COLOR VALUES ////////////////////////////////
+						   // hmin, smin, vmin hmax, smax, vmax
+vector<vector<int>> myColors{ {124,48,117,143,170,255}, // Purple
+								{68,72,156,102,126,255} };// Green
+vector<Scalar> myColorValues{ {255,0,255},		// Purple
+								{0,255,0} };// Green 	
+////////////////////////////////////////////////////////////////////
+
+Point getContours1(Mat image) {
+
+
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+
+	findContours(image, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+	//drawContours(img, contours, -1, Scalar(255, 0, 255), 2);
+	vector<vector<Point>> conPoly(contours.size());
+	vector<Rect> boundRect(contours.size());
+
+	Point myPoint(0, 0);
+
+	for (int i = 0; i < contours.size(); i++)
+	{
+		int area = contourArea(contours[i]);
+		cout << area << endl;
+
+		string objectType;
+
+		if (area > 1000)
+		{
+			float peri = arcLength(contours[i], true);
+			approxPolyDP(contours[i], conPoly[i], 0.02 * peri, true);
+
+			cout << conPoly[i].size() << endl;
+			boundRect[i] = boundingRect(conPoly[i]);
+			myPoint.x = boundRect[i].x + boundRect[i].width / 2;
+			myPoint.y = boundRect[i].y;
+
+			//drawContours(img, conPoly, i, Scalar(255, 0, 255), 2);
+			//rectangle(img, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 5);
+		}
+	}
+	return myPoint;
+}
+
+
+vector<vector<int>> findColor(Mat img)
+{
+	Mat imgHSV;
+	cvtColor(img, imgHSV, COLOR_BGR2HSV);
+
+	for (int i = 0; i < myColors.size(); i++)
+	{
+		Scalar lower(myColors[i][0], myColors[i][1], myColors[i][2]);
+		Scalar upper(myColors[i][3], myColors[i][4], myColors[i][5]);
+		Mat mask;
+		inRange(imgHSV, lower, upper, mask);
+		//imshow(to_string(i), mask);
+		Point myPoint = getContours1(mask);
+		if (myPoint.x != 0) {
+			newPoints.push_back({ myPoint.x,myPoint.y,i });
+		}
+	}
+	return newPoints;
+}
+
+void drawOnCanvas(vector<vector<int>> newPoints, vector<Scalar> myColorValues)
+{
+
+	for (int i = 0; i < newPoints.size(); i++)
+	{
+		circle(img, Point(newPoints[i][0], newPoints[i][1]), 10, myColorValues[newPoints[i][2]], FILLED);
+	}
+}
+
+
+
+void CMFCApplicationOpenCVDlg::OnBnClickedButtonVirtualPaint()
+{
+	while (true) {
+
+		cap.read(img);
+		newPoints = findColor(img);
+		drawOnCanvas(newPoints, myColorValues);
+
+		imshow("Image", img);
+		waitKey(1);
+	}
+}
+
+
+void CMFCApplicationOpenCVDlg::OnBnClickedButtonFaceRecogition()
+{
+	string path = "Resources/test.jpg";
+	Mat img = imread(path);
+
+	CascadeClassifier faceCascade;
+	faceCascade.load("Resources/haarcascade_frontalface_default.xml");
+
+	if (faceCascade.empty()) { cout << "XML file not loaded" << endl; }
+
+	vector<Rect> faces;
+	faceCascade.detectMultiScale(img, faces, 1.1, 10);
+
+	for (int i = 0; i < faces.size(); i++)
+	{
+		rectangle(img, faces[i].tl(), faces[i].br(), Scalar(255, 0, 255), 3);
+	}
+
+	imshow("Image", img);
+	waitKey(0);
+}
---
+++ resource.h
@@ -0,0 +1,31 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MFCApplicationOpenCV.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCAPPLICATION_OPENCV_DIALOG 102
+#define IDR_MAINFRAME                   128
+#define IDC_BUTTON_COLOR_PICKER         1000
+#define IDC_STATIC_TEXT                 1004
+#define IDC_EDIT1                       1008
+#define IDC_BUTTON_VIRTUAL_PAINT        1010
+#define IDC_BUTTON3                     1011
+#define IDC_BUTTON_LICENSE_PLATE_RECOGNITION 1011
+#define IDC_BUTTONDOCUMENT_SCANNER      1012
+#define IDC_BUTTON_OPEN_WEBCAM          1013
+#define IDC_BUTTON_VIDEO_OPEN           1014
+#define IDC_BUTTON_FACE_RECOGITION      1015
+#define IDC_BUTTON_IMGOPEN              1016
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
